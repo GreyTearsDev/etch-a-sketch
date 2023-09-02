367c58db4036463c31b1672e55b36e7a9a45cd22
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-09-02 17:41:04 +0100

    Get one of the divs to change background color when hovered

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-function createDivs() {
-	//Creates a div element and gives it a class name
-	let squareDivs = document.createElement('div');
-	squareDivs.className = 'grid-element';
-	squareDivs.textContent = 'hi';
+//Creates a div element and gives it a class name
+let squareDivs = document.createElement('div');
+squareDivs.className = 'grid-element';
 
-	createArrayOfDivs(squareDivs);
+
+squareDivs.addEventListener('mouseover', () => changeDivColor());
+
+createGridOfDivs();
+
+function changeDivColor() {
+	//changes the color of the div if the mouse hovers over it
+	let elements = createArrayOfDivs(squareDivs);
+	for (element in elements) {
+		elements[element].addEventListener('mouseover', () => elements[element].style.backgroundColor = 'black');
+	}
+
 }
 
 function createArrayOfDivs(div) {
 	//Creates an array of divs
-	let arrayOfDivs = []
-	for (let i = 0; i < 256; i++) {
+	let arrayOfDivs = [];
+	for (let i = 0; i < (16 * 16); i++) {
 		arrayOfDivs.unshift(div.cloneNode(true));
 	}
-	createGridOfDivs(arrayOfDivs);
+	return arrayOfDivs;
 }
 
-function createGridOfDivs(arrayOfDivs) {
+function createGridOfDivs() {
 	//Appends the divs onto the Grid container to crate the grid
+	let arrayOfDivs = createArrayOfDivs(squareDivs);
 	let gridContainer = document.body.querySelector('.grid-container');
+
 	for (element in arrayOfDivs) {
+		arrayOfDivs[element].addEventListener('mouseover', () => arrayOfDivs[element].style.backgroundColor = 'black')
 		gridContainer.appendChild(arrayOfDivs[element]);
 	}
 }
 
-createDivs()
+let hi = document.querySelector('.hi');
+hi.addEventListener('mouseover', () => hi.style.backgroundColor = 'black');
+
+
 
 
 
